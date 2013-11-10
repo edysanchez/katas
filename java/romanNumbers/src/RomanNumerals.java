716c0b2af18304d8d0d0ba98a33503519eee25e4
edysanchez <edysanchez@gmail.com>
2013-11-10 22:29:35 +0100

    Add code from kata in Barcelona dev con 2013 coding dojo

--- java/romanNumbers/src/RomanNumerals.java
+++ java/romanNumbers/src/RomanNumerals.java
@@ -1,1 +1,31 @@
+import java.util.TreeMap;
+import java.util.TreeSet;
 
+public class RomanNumerals {
+
+    public static int[] numbers = { 1, 5, 10, 50 };
+    public static String[] romans = { "I", "V", "X", "L" };
+
+    public static String translate(int number) {
+        String numberInRoman = "";
+
+        if (number == 0) {
+            return "";
+        }
+
+        for (int i = 1; i <= 3; ++i) {
+            if (number < numbers[i]) {
+                return conversion(i, number);
+            }
+        }
+
+        return numberInRoman;
+    }
+
+    private static String conversion(int pos, int number) {
+        if( numbers[pos]-number==numbers[0]) {
+            return romans[0] + translate(number+1);
+        }
+        return romans[pos-1] + translate(number - numbers[pos - 1]);
+    }
+}
--- java/romanNumbers/test/RomanNumeralsTests.java
+++ java/romanNumbers/test/RomanNumeralsTests.java
@@ -1,1 +1,51 @@
+import static org.junit.Assert.*;
 
+import org.junit.Test;
+
+
+public class RomanNumeralsTests {
+
+    @Test 
+    public void noZero() {
+        assertEquals("", RomanNumerals.translate(0));
+    }
+    
+    @Test 
+    public void one() {
+        assertEquals("I", RomanNumerals.translate(1));
+    }
+    
+    @Test 
+    public void two() {
+        assertEquals("II", RomanNumerals.translate(2));
+    }
+    
+    @Test 
+    public void three() {
+        assertEquals("III", RomanNumerals.translate(3));
+    }
+    
+    @Test
+    public void four() {
+        assertEquals("IV",RomanNumerals.translate(4));
+    }
+    
+    @Test
+    public void five() {
+        assertEquals("V",RomanNumerals.translate(5));
+    }
+    
+    @Test
+    public void six() {
+        assertEquals("VI",RomanNumerals.translate(6));
+    }
+    @Test
+    public void nine() {
+        assertEquals("IX",RomanNumerals.translate(9));
+    }  
+    @Test
+    public void ten() {
+        assertEquals("X",RomanNumerals.translate(10));
+    }
+    
+}
